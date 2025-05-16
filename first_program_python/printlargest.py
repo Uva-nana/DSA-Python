de6bf23d8f6f81834ee1622561347cd29afad93e
Uva-nana <uvananaj1l@gmail.com>
2025-05-16 16:33:50 +0530

    PrintLargestnumber

---
+++ first_program_python/printlargest.py
@@ -0,0 +1,9 @@
+#Take 2 numbers as input and print the largest number.
+
+firstnum = int(input("Enter the first num : "));
+secondnum = int(input("Enter the Second num : "))
+
+if firstnum>secondnum:
+    print("The Largest number is :"  , firstnum)
+else:
+    print("The Largest number is :" , secondnum)
