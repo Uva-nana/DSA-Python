77b97b9bb6b5d8b57790fc67479d5e696470ffbe
Uva-nana <uvananaj1l@gmail.com>
2025-05-17 11:26:53 +0530

    Whileloop

--- Loops/conditionsandloops.py
+++ Loops/conditionsandloops.py
@@ -12,3 +12,8 @@ for i in range(1,6):
 getinput = int (input("Give the number : "))
 for i in range(1,getinput+1): #5  = 0,1,2,3,4
     print(i)
+
+a= int(input())
+b= int(input())
+for i in range(a+1, b): # 2, 7  2 3,4,5,6
+    print(i)
