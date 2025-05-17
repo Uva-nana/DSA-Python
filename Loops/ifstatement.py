14663823279b22cf5491ebd2900d118fc41e82a9
Uva-nana <uvananaj1l@gmail.com>
2025-05-17 12:23:32 +0530

    Create if statement

---
+++ Loops/ifstatement.py
@@ -0,0 +1,11 @@
+a = int (input("Enter the a : "))
+b = int (input("Enter the b : "))
+c = int (input("Enter the c: "))
+
+max = c
+if a>max:
+    max = a
+if b>c:
+    max =c
+
+print("The maximum of all is :" , max)
